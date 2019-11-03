0a4344b0b58802fd1600e74c6b2ec98bf9732f36
Ben <benlaugherty@gmail.com>
2019-11-03 12:00:46 -0800

    [add] user model

--- src/web/home/models.py
+++ src/web/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    # Max length of username + #0000
+    user_name = models.CharField(max_length=37)
+    email = models.EmailField()
+    created_at = models.DateTimeField('Created At')
+
+    def __str__(self):
+        return self.user_name
--- src/web/web/settings.py
+++ src/web/web/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # My apps
+    'rest_framework',
     'api.apps.ApiConfig',
     'home.apps.HomeConfig'
 ]
