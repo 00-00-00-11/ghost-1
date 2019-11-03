9a6437b262f5edc960defae1fcb648c6c8e56816
Ben <benlaugherty@gmail.com>
2019-11-02 18:31:22 -0700

    [add] urls and basic views

--- src/web/web/urls.py
+++ src/web/web/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('home.urls')), # Home route
+    path('api/', include('api.urls')), # Api Route
     path('admin/', admin.site.urls),
 ]
