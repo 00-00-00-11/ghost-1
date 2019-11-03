5428c25aff7adeb5b4ae6b53cd3acae3e736b0dc
Ben <benlaugherty@gmail.com>
2019-11-02 17:44:00 -0700

    [add] apps, basic config, switch to postgres

--- src/web/web/settings.py
+++ src/web/web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import psycopg2
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
+    'api.apps.ApiConfig',
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ghostsite',
+        'USER': 'admin',
+        # TODO: Make sure this is secure
+        'PASSWORD': 'amazingpassword',
+        'PORT': ''
     }
 }
 
