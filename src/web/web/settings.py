1ea546e83f2e33239838e363327b300ab712e29f
Ben <benlaugherty@gmail.com>
2019-12-20 23:24:58 -0800

    [add] basic static handling, move secret key to be in environment variables

--- src/web/web/settings.py
+++ src/web/web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w3a)v=@v=2ro(71$q-_m82(inhbgy23x_q9k1#+)^tce7=-1)f'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,6 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # All Auth
 AUTHENTICATION_BACKENDS = (
