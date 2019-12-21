147481436434867ab3596d40b60f68664a4102ed
Ben <benlaugherty@gmail.com>
2019-12-20 23:45:27 -0800

    [remove] junk imports from views

--- src/web/home/views.py
+++ src/web/home/views.py
@@ -1,13 +1,4 @@
-from os import getenv
-import requests
-
-from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView
-from django.urls import reverse
-from django.http import HttpResponse
-from dotenv import load_dotenv
-load_dotenv()
-
 
 class Home(TemplateView):
     template_name = 'home/index.html'
