9216023476615fbf051865408d30e6e9aa4f8599
Ben <benlaugherty@gmail.com>
2019-12-20 23:44:11 -0800

    [remove] user model as it is handled by django-allauth

--- src/web/home/models.py
+++ src/web/home/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class User(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
-    # Max length of username + #0000
-    user_name = models.CharField(max_length=37)
-    email = models.EmailField(unique=True)
-    created_at = models.DateTimeField('Created At')
-    ip = models.GenericIPAddressField(default=None, unique=True)
-    # TODO: Token Field
-
-    def __str__(self):
-        return self.user_name
